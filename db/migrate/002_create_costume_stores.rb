faf63249bc78a4ebbc3910ecf2f631d64ddb12a4
github username <github email address>
2020-04-13 01:12:54 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -14,6 +14,3 @@ class CreateCostumeStores < ActiveRecord::Migration[6.0]
 
 end
 
-# Create your costume_stores migration here
-
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -15,6 +15,3 @@ class CreateHauntedHouses < ActiveRecord::Migration[6.0]
 
 end
 
-# Create your haunted_houses migration here
-
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
